69f309f12a6273d4efaeef0874715d5b75fdc9a5
Yuxuan (Charles) Wu <yuw095@ucsd.edu>
2023-06-17 19:02:06 -0700

    More tailorings

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
  * =============
  */
 import * as GraphAlgoViz from "./graphAlgoViz.js";
-import * as Firework from "./firework.js";
 
 /**
  * Variable declaration and initialization
@@ -16,9 +15,7 @@ var currApp;
  * ============
  */
 
-const backgroundMusic = document.querySelector("#background-music");
 const graphAlgoViz = document.querySelector("#graphAlgoViz");
-const physicsSim = document.querySelector("#physicsSim");
 const graphAlgoVizCard = document.querySelector("#graph-algo-viz-card");
 export const graphAlgoViz_tutorial_dialog_template = document.getElementById("graphAlgoViz-tutorial-template");
 export const navBar = document.querySelector("#navbar");
@@ -37,15 +34,11 @@ export const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - navBar.offsetHeight;
 
-const frameRate = 144.0;
-const frameDelay = 1000.0 / frameRate;
-
 /**
  * Bind event listeners
  * ====================
  */
-graphAlgoViz.addEventListener("click", function () {
-    ceaseCurrentProcess();
+graphAlgoVizCard.addEventListener("click", function () {
     mainExhibition.style.display = "none";
 
     navBar.style.display = "none";
@@ -69,33 +62,3 @@ graphAlgoViz.addEventListener("click", function () {
         });
     }
 });
-
-physicsSim.addEventListener("click", function () {
-    ceaseCurrentProcess();
-
-    canvas.style.display = "block";
-    Firework.bindEventlisteners();
-
-    //start the rendering process for firework
-    currApp = setInterval(function () {
-        Firework.update(frameDelay);
-    }, frameDelay);
-});
-
-graphAlgoVizCard.addEventListener("click", function () {
-    graphAlgoViz.click();
-});
-
-/**
- * Controller functions
- * ==================== 
- */
-function ceaseCurrentProcess() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    canvas.style.display = "none";
-    //remove eventlisteners for all applications
-    Firework.removeEventlisteners();
-
-    clearInterval(currApp);
-}
